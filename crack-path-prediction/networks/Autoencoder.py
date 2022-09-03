f738859c690eb05d5f6dcf9eaa1ed3aaec9c286f
saeedmhz <saeedmhz@bu.edu>
2022-09-03 18:09:30 -0400

    move Autoencoder.py to the correct folder

--- crack-path-prediction/networks/Autoencoder.py
+++ crack-path-prediction/networks/Autoencoder.py
@@ -1,101 +1,34 @@
-""" Implementation of MultiRes-WNet """
+##################################################
+## Description:
+## This script contrains PyTorch Implementation 
+## of the Autoencoder architecture used in
+## the paper
+##################################################
+## Author: Saeed Mohammadzadeh
+## Email: saeedmhz@bu.edu
+## License: 
+##################################################
 import torch
 from torch import nn
 
-
-######################
-# Creating the Model #
-######################
-class Forward_conv(nn.Module):
-    """ Multi-Resolution Convolution Block ([MultiRes-UNet Paper]), w/o Depthwise Convolution
-        input -> conv1 -> conv2 -> conv3
-        input -> conv_res
-        output: conv1 + conv2 + conv3 + conv_res
-    """
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int, stride: int, padding: int, bias: bool):
-        super(Forward_conv, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels[0], kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
-        self.bn1 = nn.BatchNorm2d(num_features=out_channels[0])
-        
-        self.conv2 = nn.Conv2d(in_channels=out_channels[0], out_channels=out_channels[1], kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
-        self.bn2 = nn.BatchNorm2d(num_features=out_channels[1])
-        
-        self.conv3 = nn.Conv2d(in_channels=out_channels[1], out_channels=out_channels[2], kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
-        self.bn3 = nn.BatchNorm2d(num_features=out_channels[2])
-        
-        self.conv_res = nn.Conv2d(in_channels=in_channels, out_channels=sum(out_channels), kernel_size=1, stride=1, padding=0, bias=bias)
-        self.bn_res = nn.BatchNorm2d(num_features=sum(out_channels))
-
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        x1 = self.relu(self.bn1(self.conv1(x)))
-        x2 = self.relu(self.bn2(self.conv2(x1)))
-        x3 = self.relu(self.bn3(self.conv3(x2)))
-
-        X = torch.cat((x1, x2, x3), dim=1)
-        Xres = self.relu(self.bn_res(self.conv_res(x)))
-
-        out = Xres + X
-
-        return out
-
-
-class ForwardDepthwise(nn.Module):
-    """ Multi-Resolution Convolution Block ([MultiRes-UNet Paper]), w/ Depthwise Convolutions
-        input -> conv1 -> conv2 -> conv3
-        input -> conv_res
-        output: conv1 + conv2 + conv3 + conv_res
-    """
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias):
-        super(ForwardDepthwise, self).__init__()
-
-        self.conv1 = DepthwiseConv2d(in_channels=in_channels, out_channels=out_channels[0], kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
-        self.bn1 = nn.BatchNorm2d(num_features=out_channels[0])
-        
-        self.conv2 = DepthwiseConv2d(in_channels=out_channels[0], out_channels=out_channels[1], kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
-        self.bn2 = nn.BatchNorm2d(num_features=out_channels[1])
-        
-        self.conv3 = DepthwiseConv2d(in_channels=out_channels[1], out_channels=out_channels[2], kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
-        self.bn3 = nn.BatchNorm2d(num_features=out_channels[2])
-        
-        self.conv_res = nn.Conv2d(in_channels=in_channels, out_channels=sum(out_channels), kernel_size=1, stride=1, padding=0, bias=bias)
-        self.bn_res = nn.BatchNorm2d(num_features=sum(out_channels))
-
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        x1 = self.relu(self.bn1(self.conv1(x)))
-        x2 = self.relu(self.bn2(self.conv2(x1)))
-        x3 = self.relu(self.bn3(self.conv3(x2)))
-        
-        X = torch.cat((x1, x2, x3), dim=1)
-        Xres = self.relu(self.bn_res(self.conv_res(x)))
-
-        out = Xres + X
-    
-        return out
-
-
-class DepthwiseConv2d(nn.Module):
-    """ Replace a regular conv layer with a channel-wise conv layer followed by a regular conv with kernel_size = 1
-    """
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias):
-        super(DepthwiseConv2d, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias, groups=in_channels)
-        self.conv2 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0, bias=bias, groups=1)
-
-    def forward(self, x):
-        x = self.conv1(x)
-
-        out = self.conv2(x)
-
-        return out
+from BuildingBlocks import Forward_conv, ForwardDepthwise
 
 
 class Encoder(nn.Module):
+    """The encoder section of our Autoencoder
+
+    Parameters
+    ----------
+    in_channels : int
+        Channel number of the input tensor
+    c : int
+        Channel number of the output of the first layer encoder
+    k : int
+        Size of convolutional kernels used
+    
+    input --> encoder --> encoded_info
+
+    """
     def __init__(self, c, k, in_channels):
         super(Encoder, self).__init__()
 
@@ -128,6 +61,20 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
+    """The decoder section of our Autoencoder
+
+    Parameters
+    ----------
+    in_channels : int
+        Channel number of the input tensor
+    c : int
+        Channel number of the output of the first layer encoder
+    k : int
+        Size of convolutional kernels used
+    
+    encoded_info --> decoder --> output
+
+    """
     def __init__(self, c, k, in_channels):
         super(Decoder, self).__init__()
 
